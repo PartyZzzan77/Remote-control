c367c945fd257dc228239cb6f399f38a3ca372be
Mitya <partyzan327@gmail.com>
2022-12-29 12:35:21 +0300

    refactor: remove dependencies within the terms of reference

--- src/handlers/printScreen.ts
+++ src/handlers/printScreen.ts
@@ -1,19 +1,13 @@
-import { mouse } from '@nut-tree/nut-js';
 import Jimp from 'jimp';
-import robot from 'robotjs';
 import { Constants } from '../utils/constants';
-import { scanImage } from '../utils/helpers/index.js';
 
 export const printScreen = async () => {
     try {
         const w = Constants.SCREEN_SHOT_WIDTH;
         const h = Constants.SCREENSHOT_HEIGHT;
-        const { x, y } = await mouse.getPosition();
-        const bitmap = robot.screen.capture(x, y, w, h);
-        const buffer = await scanImage(bitmap);
-        const getBase64 = await buffer.getBase64Async(buffer.getMIME());
+        const jArea = new Jimp(w, h);
 
-        return getBase64.split(',')[1];
+        return (await jArea.getBase64Async(jArea.getMIME())).split(',')[1];
     } catch (err) {
         return console.error(err);
     }
--- src/utils/helpers/index.ts
+++ src/utils/helpers/index.ts
@@ -1,4 +1,3 @@
 export { printWSParams } from './printWSParams.js';
 export { pressLeftBTNMouse } from './pressLeftBTNMouse.js';
 export { releaseLeftBTNMouse } from './releaseLeftBTNMouse.js';
-export { scanImage } from './scanImage.js';
