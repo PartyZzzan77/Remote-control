faa7ee1dad5c38b4171868eaf4e864fd5d86e965
Mitya <partyzan327@gmail.com>
2022-12-30 20:10:35 +0300

    refactor: drawRectangle

--- src/handlers/drawRectangle.ts
+++ src/handlers/drawRectangle.ts
@@ -6,17 +6,15 @@ export const drawRectangle = async (offset: string[]): Promise<void> => {
     const [w, h] = offset;
     let width = parseInt(w);
     const height = parseInt(h);
+
     width = width === height ? height * 2 : width;
 
     if (w && h) {
+        mouse.config.mouseSpeed = Constants.MOUSE_SPEED;
         await pressLeftBTNMouse();
-        mouse.config.mouseSpeed = Constants.MOUSE_SPEED;
         await mouse.move(up(height));
-        mouse.config.mouseSpeed = Constants.MOUSE_SPEED;
         await mouse.move(right(width));
-        mouse.config.mouseSpeed = Constants.MOUSE_SPEED;
         await mouse.move(down(height));
-        mouse.config.mouseSpeed = Constants.MOUSE_SPEED;
         await mouse.move(left(width));
         await releaseLeftBTNMouse();
     }
