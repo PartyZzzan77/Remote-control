67062199db3fc4a9e0bfb2d5194173bdc61c2e8b
Mitya <partyzan327@gmail.com>
2022-12-30 20:07:23 +0300

    refactor: drawCircle take the check out of the loop

--- src/handlers/drawCircle.ts
+++ src/handlers/drawCircle.ts
@@ -2,24 +2,24 @@ import { mouse, straightTo, Point } from '@nut-tree/nut-js';
 import { Constants } from '../utils/constants';
 import { pressLeftBTNMouse, releaseLeftBTNMouse } from '../utils/helpers/index.js';
 
-
 export const drawCircle = async (offset: string[]): Promise<void> => {
-    const { x, y } = await mouse.getPosition();
     const [radius] = offset;
     const intRadius = parseInt(radius);
-    const intX = parseInt(String(x));
-    const intY = parseInt(String(y));
 
-    if (radius && x && y) {
+    if (radius) {
+        const { x, y } = await mouse.getPosition();
+        const intX = parseInt(String(x));
+        const intY = parseInt(String(y));
         await pressLeftBTNMouse();
+
         for (let i = 0; i <= Math.PI * 2; i += 0.01) {
             const moveX = intX + (intRadius * Math.cos(i));
             const moveY = intY + (intRadius * Math.sin(i));
             const target = new Point(moveX, moveY);
-
             mouse.config.mouseSpeed = Constants.MOUSE_SPEED;
             await mouse.move(straightTo(target));
         }
+
         await releaseLeftBTNMouse();
     }
 };
