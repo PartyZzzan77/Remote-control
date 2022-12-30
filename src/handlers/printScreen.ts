9234851dacb28e3edd8ba11944ab8490486bf95f
Mitya <partyzan327@gmail.com>
2022-12-30 20:03:20 +0300

    refactor: drawSquare&printScreen functions

--- src/handlers/printScreen.ts
+++ src/handlers/printScreen.ts
@@ -2,11 +2,17 @@ import fs from 'node:fs/promises';
 import { screen } from '@nut-tree/nut-js';
 
 export const printScreen = async () => {
-    const screenName = 'print_screen.png';
-    //captureRegion causes an error at the library level in the Mac OS system -> please understand the principle is clear
-    const screenShotPath = await screen.capture(screenName);
-    const base64 = await fs.readFile(screenShotPath, 'base64');
+    try {
+        const screenName = 'print_screen.png';
+        //captureRegion causes an error at the library level in the Mac OS system -> please understand the principle is clear
+        const screenShotPath = await screen.capture(screenName);
+        const base64 = await fs.readFile(screenShotPath, 'base64');
 
-    fs.unlink(screenShotPath);
-    return `prnt_scrn ${base64}`;
+        fs.unlink(screenShotPath);
+        return `prnt_scrn ${base64}`;
+    } catch (err) {
+        if (err instanceof Error) {
+            console.error(err.message);
+        }
+    }
 };
